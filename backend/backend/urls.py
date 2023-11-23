801434ce19f0860f6bd36459925af03a64d1be88
MaksymMalicki <maksym.malicki.2115@gmail.com>
2023-11-23 10:57:50 +0100

    urls cleanups

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import (
     searchAppointmentView,
     patientCancelAppointmentView,
-    viewDoctorDataView,
     patientProfileView,
     patientRegistrationView,
     patientLogoutView,
